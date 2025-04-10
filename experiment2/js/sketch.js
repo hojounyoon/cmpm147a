12f7337ca95dabcfca51aeb3ecdbf722cb5b43d2
hojounyoon <140112030+hojounyoon@users.noreply.github.com>
2025-04-10 15:14:48 -0700

    Update sketch.js

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -51,8 +51,25 @@ function setup() {
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  randomSeed(seed);
-  background(135, 206, 235);
+  background(220);    
+
+  // call a method on the instance
+  myInstance.myMethod();
+
+  // Set up rotation for the rectangle
+  push(); // Save the current drawing context
+  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
+  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
+  fill(234, 31, 81);
+  noStroke();
+  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
+  pop(); // Restore the original drawing context
+
+  // The text is not affected by the translate and rotate
+  fill(255);
+  textStyle(BOLD);
+  textSize(140);
+  text("p5*", centerHorz - 105, centerVert + 40);
     
   // Ocean
   noStroke();
