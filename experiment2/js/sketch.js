0a4a0e328b6a64aea18abf0faf629889f692e74d
hojounyoon <140112030+hojounyoon@users.noreply.github.com>
2025-04-10 14:59:16 -0700

    Update sketch.js

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -33,7 +33,7 @@ function resizeScreen() {
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
   // redrawCanvas(); // Redraw everything based on new size
 }
-let seed = 0;
+
 // setup() function is called once when the program starts
 function setup() {  
   canvasContainer = $("#canvas-container");
