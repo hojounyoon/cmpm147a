666ad94abebc894415d69b9596542cef452dfaed
hojounyoon <140112030+hojounyoon@users.noreply.github.com>
2025-04-10 17:13:04 -0700

    Update sketch.js

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -37,8 +37,6 @@ function resizeScreen() {
 
 // setup() function is called once when the program starts
 function setup() {
-  createCanvas(400, 200);
-  createButton("reimagine").mousePressed(() => seed++);
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
