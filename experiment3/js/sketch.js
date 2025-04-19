b1c4811c6e07d6f5f3e6ebee6836abfc4f00a402
hojounyoon <140112030+hojounyoon@users.noreply.github.com>
2025-04-19 00:01:56 -0700

    Update sketch.js

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -41,10 +41,10 @@ function setup() {
   myInstance = new MyClass("VALUE1", "VALUE2");
 
   // Generate the dungeon grid
-  grid = generateGrid(40, 30); // Size of the dungeon grid
+  grid = generateGrid(85, 35); // Size of the dungeon grid
 
   createButton("Reseed").mousePressed(() => {
-    grid = generateGrid(40, 30); // regenerate with same size
+    grid = generateGrid(85, 35); // regenerate with same size
   });
 
   $(window).resize(function() {
