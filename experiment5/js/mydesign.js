55b96c100cb6902b4cacd811830ec8e25746679a
hojounyoon <140112030+hojounyoon@users.noreply.github.com>
2025-05-01 12:34:42 -0700

    Update mydesign.js

--- experiment5/js/mydesign.js
+++ experiment5/js/mydesign.js
@@ -7,31 +7,21 @@ function getInspirations() {
       name: "Lunch atop a Skyscraper", 
       assetUrl: "img/lunch-on-a-skyscraper.jpg",
       credit: "Lunch atop a Skyscraper, Charles Clyde Ebbets, 1932"
-      shape: "rect"
     },
     {
       name: "Train Wreck", 
       assetUrl: "img/train-wreck.jpg",
       credit: "Train Wreck At Monteparnasse, Levy & fils, 1895"
-      shape: "rect"
     },
     {
       name: "Migrant mother", 
       assetUrl: "img/migrant-mother.jpg",
       credit: "Migrant Mother near Nipomo, California, Dorothea Lange, 1936"
-      shape: "ellipse"
     },
     {
       name: "Disaster Girl", 
       assetUrl: "img/girl-with-fire.jpg",
       credit: "Four-year-old Zoë Roth, 2005"
-      shape: "ellipse"
-    },
-    {
-      name: "Santa Cruz Sunset", 
-      assetUrl: "https://cdn.glitch.global/ef4f43ca-7b3d-40e1-8773-aa2c29209ecf/walton-lighthouse-sunset-1.jpg?v=1746123746475",
-      credit: "Santa Cruz Lighthouse",
-      shape: "rect"
     },
   ];
 }
@@ -53,37 +43,26 @@ function initDesign(inspiration) {
   
   let design = {
     bg: 128,
-    shapeCount: 100,
-    maxWidth: width / 2,
-    maxHeight: height / 2,
-    fg: [],
-  };
+    fg: []
+  }
   
-  for (let i = 0; i < design.shapeCount; i++) {
-    design.fg.push({
-      x: random(width),
-      y: random(height),
-      w: random(design.maxWidth),
-      h: random(design.maxHeight),
-      fill: random(255)
-    });
+  for(let i = 0; i < 100; i++) {
+    design.fg.push({x: random(width),
+                    y: random(height),
+                    w: random(width/2),
+                    h: random(height/2),
+                    fill: random(255)})
   }
   return design;
 }
 
 function renderDesign(design, inspiration) {
-  image(inspiration.image, 0, 0, width, height);
-  fill(design.bg, 128);
-  rect(0, 0, width, height);
-
+  
+  background(design.bg);
   noStroke();
-  for (let box of design.fg) {
+  for(let box of design.fg) {
     fill(box.fill, 128);
-    if (inspiration.shape === "ellipse") {
-      ellipse(box.x, box.y, box.w, box.h);
-    } else {
-      rect(box.x, box.y, box.w, box.h);
-    }
+    rect(box.x, box.y, box.w, box.h);
   }
 }
 
@@ -100,5 +79,5 @@ function mutateDesign(design, inspiration, rate) {
 
 
 function mut(num, min, max, rate) {
-    return constrain(randomGaussian(num, (rate * (max - min)) / 20), min, max);
+    return constrain(randomGaussian(num, (rate * (max - min)) / 10), min, max);
 }
